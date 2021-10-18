2b340cf5549574de2864c06571ce07390e299758
mushipeas <8151412+mushipeas@users.noreply.github.com>
2021-10-18 00:13:47 +0000

    update tests for data service using new dao

--- src/test/java/com/mushipeas/coronavirustracker/CoronavirusDataServiceTest.java
+++ src/test/java/com/mushipeas/coronavirustracker/CoronavirusDataServiceTest.java
@@ -1,90 +1,51 @@
 package com.mushipeas.coronavirustracker;
 
+import com.mushipeas.coronavirustracker.daos.CoronavirusDAO;
 import com.mushipeas.coronavirustracker.services.CoronavirusDataService;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-// @ExtendWith(MockitoExtension.class)
-@ExtendWith(SpringExtension.class)
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 class CoronavirusDataServiceTest {
-     
-    private static CoronavirusDataService coronavirusDataService;
- 
-    @BeforeAll
-    static void setUp() {
-        coronavirusDataService = new CoronavirusDataService();
-        coronavirusDataService.fetchCovidData();
-    }
     
-    @Test
-    void fetchCovidDataTest_success() {
-        // TODO The fetch function either needs to be moved to a DAO or properly tested here
-        assertThat(coronavirusDataService.getAllLocationStats()).isNotNull();
-    }
+    @Mock private CoronavirusDAO coronavirusDAO;
+    private CoronavirusDataService underTest;
 
+    @BeforeEach
+    void setUp() {
+        underTest = new CoronavirusDataService(coronavirusDAO);
+    }
 
     @Test
-    void getAllLocationStatsTest() {
-        assertThat(coronavirusDataService.getAllLocationStats()).isNotNull();
+    void fetchesAllCovidData() {
+        // when
+        underTest.getAllLocationStats();
+        // then
+        verify(coronavirusDAO).selectAllLocationStats();
     }
 
-    
+
     @Test
-    void getTotalReportedCasesTest() {
-        assertThat(coronavirusDataService.getTotalReportedCases()).isGreaterThan(0);
+    void fetchesTotalReportedCases() {
+        // when
+        underTest.getTotalReportedCases();
+        // then
+        verify(coronavirusDAO).calculateTotalReportedCases();
     }
-    
+
+
     @Test
-    void getNewCasesTodayTest() {
-        assertThat(coronavirusDataService.getNewCasesToday()).isGreaterThan(0);
+    void fetchesNewCasesToday() {
+        // when
+        underTest.getNewCasesToday();
+        // then
+        verify(coronavirusDAO).calculateNewCasesToday();
     }
 
-
-    // @Test
-    // public void getAllEmployeesTest()
-    // {
-    //     List<EmployeeVO> list = new ArrayList<EmployeeVO>();
-    //     EmployeeVO empOne = new EmployeeVO(1, "John", "John", "howtodoinjava@gmail.com");
-    //     EmployeeVO empTwo = new EmployeeVO(2, "Alex", "kolenchiski", "alexk@yahoo.com");
-    //     EmployeeVO empThree = new EmployeeVO(3, "Steve", "Waugh", "swaugh@gmail.com");
-         
-    //     list.add(empOne);
-    //     list.add(empTwo);
-    //     list.add(empThree);
-         
-    //     when(dao.getEmployeeList()).thenReturn(list);
-         
-    //     //test
-    //     List<EmployeeVO> empList = manager.getEmployeeList();
-         
-    //     assertEquals(3, empList.size());
-    //     verify(dao, times(1)).getEmployeeList();
-    // }
-     
-    // @Test
-    // public void getEmployeeByIdTest()
-    // {
-    //     when(dao.getEmployeeById(1)).thenReturn(new EmployeeVO(1,"Lokesh","Gupta","user@email.com"));
-         
-    //     EmployeeVO emp = manager.getEmployeeById(1);
-         
-    //     assertEquals("Lokesh", emp.getFirstName());
-    //     assertEquals("Gupta", emp.getLastName());
-    //     assertEquals("user@email.com", emp.getEmail());
-    // }
-     
-    // @Test
-    // public void createEmployeeTest()
-    // {
-    //     EmployeeVO emp = new EmployeeVO(1,"Lokesh","Gupta","user@email.com");
-         
-    //     manager.addEmployee(emp);
-         
-    //     verify(dao, times(1)).addEmployee(emp);
-    // }
-    
 }
